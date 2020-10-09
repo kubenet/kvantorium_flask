0ffb1e30e5cf8a9409b3a44ab0fa87a468ac9cab
Student <Student@quantorium70.ru>
2020-10-09 19:42:08 +0700

    Initial commit. My fist commit.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    print(request)
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return '<h3>About</h3>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
